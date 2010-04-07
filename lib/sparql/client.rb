eaf1795d21142f78b67c36362dde20a8111f7978
Arto Bendiken <arto@bendiken.net>
2010-04-07 08:11:40 +0200

    Implemented SPARQL::Client#initialize.

--- lib/sparql/client.rb
+++ lib/sparql/client.rb
@@ -1,8 +1,22 @@
+require 'json'
+require 'rdf'
 require 'sparql/version'
 
 module SPARQL
   ##
   class Client
-    # TODO
+    ##
+    # @param  [String, #to_s]          url
+    # @param  [Hash{Symbol => Object}] options
+    def initialize(url, options = {}, &block)
+      @url, @options = url, options
+
+      if block_given?
+        case block.arity
+          when 1 then block.call(self)
+          else instance_eval(&block)
+        end
+      end
+    end
   end
 end
