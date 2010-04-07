dae5da4c63f172a729bb80e106d51b48ed1fcd22
Arto Bendiken <arto@bendiken.net>
2010-04-07 07:44:00 +0200

    Reset the version number back to square one.

--- lib/sparql/version.rb
+++ lib/sparql/version.rb
@@ -1,9 +1,23 @@
-module SPARQL
+module SPARQL; module Client
   module VERSION
     MAJOR = 0
     MINOR = 0
-    TINY  = 1
+    TINY  = 0
+    EXTRA = :pre
 
     STRING = [MAJOR, MINOR, TINY].join('.')
+    STRING << ".#{EXTRA}" if EXTRA
+
+    ##
+    # @return [String]
+    def self.to_s() STRING end
+
+    ##
+    # @return [String]
+    def self.to_str() STRING end
+
+    ##
+    # @return [Array(Integer, Integer, Integer)]
+    def self.to_a() [MAJOR, MINOR, TINY] end
   end
-end
+end; end
