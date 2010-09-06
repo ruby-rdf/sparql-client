525a0f4b352973e078a25111d0c303348ccabc95
Arto Bendiken <arto@bendiken.net>
2010-09-06 10:53:51 +0200

    Ensured lib/sparql.rb is identical to the corresponding file in the SPARQL::Grammar gem.

--- lib/sparql.rb
+++ lib/sparql.rb
@@ -1,3 +1,6 @@
 module SPARQL
-  autoload :Client, 'sparql/client'
+  # @see http://rubygems.org/gems/sparql-client
+  autoload :Client,  'sparql/client'
+  # @see http://rubygems.org/gems/sparql-grammar
+  autoload :Grammar, 'sparql/grammar'
 end
