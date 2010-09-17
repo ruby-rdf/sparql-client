ad0ffc43fa565526d083728f9047f2aaf490723c
Arto Bendiken <arto@bendiken.net>
2010-09-17 19:22:09 +0200

    Bumped the version to 0.0.6.

--- lib/sparql/client/version.rb
+++ lib/sparql/client/version.rb
@@ -2,7 +2,7 @@ module SPARQL; class Client
   module VERSION
     MAJOR = 0
     MINOR = 0
-    TINY  = 5
+    TINY  = 6
     EXTRA = nil
 
     STRING = [MAJOR, MINOR, TINY, EXTRA].compact.join('.')
