bbde469f40f5c78e1741f5802be0730df4adc3aa
Arto Bendiken <arto@bendiken.net>
2010-11-19 16:58:31 +0100

    Ensured that lib/sparql.rb is identical to the corresponding file in the SPARQL::Algebra gem.

--- lib/sparql.rb
+++ lib/sparql.rb
@@ -1,6 +1,8 @@
 module SPARQL
-  # @see http://rubygems.org/gems/sparql-client
-  autoload :Client,  'sparql/client'
+  # @see http://rubygems.org/gems/sparql-algebra
+  autoload :Algebra, 'sparql/algebra'
   # @see http://rubygems.org/gems/sparql-grammar
   autoload :Grammar, 'sparql/grammar'
+  # @see http://rubygems.org/gems/sparql-client
+  autoload :Client,  'sparql/client'
 end
