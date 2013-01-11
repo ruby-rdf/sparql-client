8108a85b250b3fe0f8731e422b869cee4b90537a
Arto Bendiken <arto@bendiken.net>
2013-01-11 06:57:17 +0100

    Ensured that the SPARQL::Client specs still pass after recent changes.

--- spec/client_spec.rb
+++ spec/client_spec.rb
@@ -5,7 +5,7 @@ describe SPARQL::Client do
   describe "when querying" do
     before(:each) do
       @client = SPARQL::Client.new('http://data.linkedmdb.org/sparql')
-      @query = 'DESCRIBE ?kb WHERE  { ?kb <http://data.linkedmdb.org/resource/movie/actor_name> "Kevin Bacon" . }'
+      @query = 'DESCRIBE ?kb WHERE { ?kb <http://data.linkedmdb.org/resource/movie/actor_name> "Kevin Bacon" . }'
     end
 
     def response(header)
@@ -16,54 +16,54 @@ describe SPARQL::Client do
     end
 
     it "should handle successful response with plain header" do
-      @client.should_receive(:get).and_yield response('text/plain')
-      RDF::Reader.should_receive(:for).with(:content_type=>'text/plain')
+      @client.should_receive(:request).and_yield response('text/plain')
+      RDF::Reader.should_receive(:for).with(:content_type => 'text/plain')
       @client.query(@query)
     end
 
     it "should handle successful response with boolean header" do
-      @client.should_receive(:get).and_yield response(SPARQL::Client::RESULT_BOOL)
+      @client.should_receive(:request).and_yield response(SPARQL::Client::RESULT_BOOL)
       @client.query(@query).should == false
     end
 
-    it "should handle successful response with json header" do
-      @client.should_receive(:get).and_yield response(SPARQL::Client::RESULT_JSON)
+    it "should handle successful response with JSON header" do
+      @client.should_receive(:request).and_yield response(SPARQL::Client::RESULT_JSON)
       @client.class.should_receive(:parse_json_bindings)
       @client.query(@query)
     end
 
-    it "should handle successful response with xml header" do
-      @client.should_receive(:get).and_yield response(SPARQL::Client::RESULT_XML)
+    it "should handle successful response with XML header" do
+      @client.should_receive(:request).and_yield response(SPARQL::Client::RESULT_XML)
       @client.class.should_receive(:parse_xml_bindings)
       @client.query(@query)
     end
 
-    it "should handle successful response with overridden xml header" do
-      @client.should_receive(:get).and_yield response(SPARQL::Client::RESULT_XML)
+    it "should handle successful response with overridden XML header" do
+      @client.should_receive(:request).and_yield response(SPARQL::Client::RESULT_XML)
       @client.class.should_receive(:parse_json_bindings)
-      @client.query(@query, :content_type=>SPARQL::Client::RESULT_JSON)
+      @client.query(@query, :content_type => SPARQL::Client::RESULT_JSON)
     end
 
-    it "should handle successful response with overridden json header" do
-      @client.should_receive(:get).and_yield response(SPARQL::Client::RESULT_JSON)
+    it "should handle successful response with overridden JSON header" do
+      @client.should_receive(:request).and_yield response(SPARQL::Client::RESULT_JSON)
       @client.class.should_receive(:parse_xml_bindings)
-      @client.query(@query, :content_type=>SPARQL::Client::RESULT_XML)
+      @client.query(@query, :content_type => SPARQL::Client::RESULT_XML)
     end
 
     it "should handle successful response with overridden plain header" do
-      @client.should_receive(:get).and_yield response('text/plain')
-      RDF::Reader.should_receive(:for).with(:content_type=>'text/turtle')
-      @client.query(@query, :content_type=>'text/turtle')
+      @client.should_receive(:request).and_yield response('text/plain')
+      RDF::Reader.should_receive(:for).with(:content_type => 'text/turtle')
+      @client.query(@query, :content_type => 'text/turtle')
     end
 
     it "should handle successful response with custom headers" do
-      @client.should_receive(:get).with(anything, "Authorization" => "Basic XXX==").
+      @client.should_receive(:request).with(anything, "Authorization" => "Basic XXX==").
         and_yield response('text/plain')
       @client.query(@query, :headers => {"Authorization" => "Basic XXX=="})
     end
 
     it "should handle successful response with initial custom headers" do
-      options = {:headers => {"Authorization" => "Basic XXX=="}}
+      options = {:headers => {"Authorization" => "Basic XXX=="}, :method => :get}
       @client = SPARQL::Client.new('http://data.linkedmdb.org/sparql', options)
       @client.instance_variable_set :@http, mock(:request => response('text/plain'))
       Net::HTTP::Get.should_receive(:new).with(anything, hash_including(options[:headers]))
